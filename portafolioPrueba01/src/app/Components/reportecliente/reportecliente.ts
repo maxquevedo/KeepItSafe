af01f904328597531abaad8d75fa610e67186453
Christian Castillo <ch.castillog@alumnos.duoc.cl>
2020-12-13 23:44:00 -0300

    Reportes

---
+++ portafolioPrueba01/src/app/Components/reportecliente/reportecliente.ts
@@ -0,0 +1,21 @@
+export class reportecliente {
+    
+    USR_ID?;
+    USR_USERNAME?;
+    USR_CORREO?;
+    USR_NOMBRECOMPLETO?;
+    USR_PASSWORD?;
+    USR_TIPOUSUARIO?;
+    USR_IDPERFIL?;
+
+    REP_CLI_ID?;
+    REP_ID_CLI?;
+    REP_FECHA?;
+    REP_VISITAS?;
+    REP_ASESORIAS?;
+    REP_PROPUESTAS?;
+    REP_ACCIDENTES?;
+    REP_PROFESIONAL?;
+    REP_CAPACITACIONES?;
+
+}
