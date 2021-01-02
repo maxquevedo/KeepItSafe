8a0c75cc12523b9854430ad1eed2918ba4f98ba3
Juan Araus <jarausq@gmail.com>
2021-01-02 08:53:43 -0300

    Mantenedor Clientes

--- portafolioPrueba01/src/app/Components/clientes/cliente.service.ts
+++ portafolioPrueba01/src/app/Components/clientes/cliente.service.ts
@@ -25,38 +25,16 @@ export class ClienteService {
   }
 
   getCliente(USR_ID){
-    return this.http.get(`${this.env.apiUrl}/web/clientes${USR_ID}`);
+    return this.http.get(`${this.env.apiUrl}/web/cliente/${USR_ID}`);
   }
 
   update(USR_ID:string,cliente){
-    return this.http.put(`${this.env.apiUrl}/web/Clienes/${USR_ID}`,Cliente);
+    return this.http.put(`${this.env.apiUrl}/web/Cliente/${USR_ID}`,cliente);
   }
 
   delete(USR_ID: number){
-    return this.http.delete(`${this.env.apiUrl}/web/usuario/${USR_ID}`);
+    return this.http.put(`${this.env.apiUrl}/web/usuario/${USR_ID}`,{headers: this.httpHeaders});
   }
   
-  /*
-    getClientes(): Observable<Cliente[]> {
-
-      return this.http.get<Cliente[]>(this.env.apiUrl);
-      }
-
-      create(cliente: Cliente): Observable<Cliente>{
-        return this.http.post<Cliente>(this.env.apiUrl, cliente, {headers: this.httpHeaders});
-      }
-
-      getCliente(USR_ID): Observable<Cliente>{
-        return this.http.get<Cliente>(`${this.env.apiUrl}/${USR_ID}`);
-      }
-
-      update(cliente: Cliente): Observable<Cliente>{
-        return this.http.put<Cliente>(`${this.env.apiUrl}/${cliente.USR_ID}`, cliente, {headers: this.httpHeaders});
-      }
-
-      delete(USR_ID: number): Observable<Cliente>{
-        return this.http.delete<Cliente>(`${this.env.apiUrl}/${USR_ID}`, {headers: this.httpHeaders});
-      }
-*/
   }
 
